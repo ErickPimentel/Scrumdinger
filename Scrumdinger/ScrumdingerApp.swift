0b4b6fff8a991e73ce9f3865b3ef3e575377e05e
ErickPimentel <erickspinellipimentel@gmail.com>
2022-06-21 20:09:08 -0300

    Section 1: Create a Project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by ErickPimentel on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
