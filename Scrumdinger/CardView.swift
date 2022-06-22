a230871a2411f8651c82bdd095b40d4b50068dbf
ErickPimentel <erickspinellipimentel@gmail.com>
2022-06-22 18:07:57 -0300

    Section 4: Customize the Label Style

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct CardView: View {
     let scrum: DailyScrum
+    
     var body: some View {
-        VStack(alignment: .leading){
+        VStack(alignment: .leading) {
             Text(scrum.title)
+                .accessibilityAddTraits(.isHeader)
                 .font(.headline)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+                    .labelStyle(TrailingIconLabelStyle.init())
             }
             .font(.caption)
         }
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by ErickPimentel on 22/06/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
+
